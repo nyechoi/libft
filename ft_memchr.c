2c97808ee5171d857242c1629495fa65b5790e44
Nathalia Ye Eun Choi <nye-eun-@c1r2p3.42sp.org.br>
2022-09-26 15:29:03 -0300

    added gitignore

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: nye-eun- <nye-eun-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 23:38:10 by nye-eun-          #+#    #+#             */
-/*   Updated: 2022/09/26 20:19:20 by nye-eun-         ###   ########.fr       */
+/*   Updated: 2022/09/26 15:28:00 by nye-eun-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*void	*ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	while (n--)
 	{
@@ -21,17 +21,5 @@
 		s++;
 	}
 
-	return (NULL);
-}*/
-
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	size_t	i;
-
-	s--;
-	i = -1;
-	while (++i < n)
-		if (*(unsigned char *)++s == (unsigned char)c)
-			return ((unsigned char *)s);
 	return (NULL);
 }
